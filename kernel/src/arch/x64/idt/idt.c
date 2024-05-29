a4bf1b80f764d4262f11b10cc390a001a08fdbda
Raphael Wegmann <raphael.wegmann81@gmail.com>
2024-05-29 17:40:59 +0200

    broken: migrated ISR routines to Assembly

--- kernel/src/arch/x64/idt/idt.c
+++ kernel/src/arch/x64/idt/idt.c
@@ -6,7 +6,9 @@
 
   Structs:   
 
-  Functions: 
+  Functions: interrupt_handler: The interrupt handler
+             __priv_init_entry: Initialize an IDT entry
+             init_idt: Initialize the IDT
 
   Defines:   
              
@@ -17,6 +19,11 @@
 #include <kernel.h>
 
 void interrupt_handler() {
+
+    uint64_t vector = 0;
+
+    asm ("pop %0" : "=r"(vector));
+
     printf("Interrupt!\n");
 
     hlt();
@@ -36,6 +43,8 @@ idt_entry_t __priv_init_entry(uint64_t base, uint8_t flags) {
 
 aligned(0x10) static idt_entry_t idt_entries[IDT_ENTRIES];
 
+extern void *isr_stub_table[256];
+
 void init_idt() {
     idt_ptr_t idt_p = (idt_ptr_t){
         .limit = sizeof(idt_entry_t) * IDT_ENTRIES - 1,
@@ -43,11 +52,11 @@ void init_idt() {
     };
 
     for (uint16_t i = 0; i < IDT_ENTRIES; i++) {
-        idt_entries[i] = __priv_init_entry((uintptr_t)&interrupt_handler, 0x8E);
+        idt_entries[i] = __priv_init_entry((uintptr_t)isr_stub_table[i], 0x8E);
     }
 
     asm("lidt %0" : : "m"(idt_p) : "memory");
     asm("sti");
 
-    printf("IDT Initialized. Base: 0x%p, Limit: 0x%X\n", (void*)idt_p.base, idt_p.limit);
+    ok("IDT Initialized. Base: 0x%p, Limit: 0x%X\n", (void*)idt_p.base, idt_p.limit);
 }
